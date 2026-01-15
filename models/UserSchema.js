c6d494753aae10a02a24d0b2bb0fd76ab2392915
saksham garg <59284238+sakshamceo@users.noreply.github.com>
2026-01-15 13:56:16 +0530

    Fix typo in 'required' field for email

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        requiredd: true,
+        required: true,
         unique: true
     },
     password: {
